2c74192fd6def698e0159c45081260443ca3cf83
Arysage8 <74323343+Arysage8@users.noreply.github.com>
2025-01-18 14:27:14 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+let currentIndex = 0;
+
+const carouselImages = document.querySelectorAll('.carousel img');
+const totalImages = carouselImages.length;
+
+function showNextImage() {
+    currentIndex = (currentIndex + 1) % totalImages;
+    updateCarouselPosition();
+}
+
+function showPrevImage() {
+    currentIndex = (currentIndex - 1 + totalImages) % totalImages;
+    updateCarouselPosition();
+}
+
+function updateCarouselPosition() {
+    const offset = -currentIndex * 100;
+    document.querySelector('.carousel').style.transform = `translateX(${offset}%)`;
+}
+
+// Cambiar imagen cada 3 segundos
+setInterval(showNextImage, 3000);
